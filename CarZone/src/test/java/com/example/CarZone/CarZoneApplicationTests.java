6e9f34d908dfd71151819b0f9226aa75bfea0da2
ankitbisen <bisenankit21@gmail.com>
2025-09-02 22:24:48 +0530

    Initial commit with multiple Spring Boot projects

---
+++ CarZone/src/test/java/com/example/CarZone/CarZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
